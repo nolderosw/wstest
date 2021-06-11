b0090c9fc1cbffe02eae9f643fadfebff03299af
Palmeira, Wesley W <wesley.w.palmeira@accenture.com>
2021-06-11 17:16:51 -0300

    teste https

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const fs = require('fs');
-const https = require('https');
-const WebSocket = require('ws');
+const fs = require('fs')
+const https = require('https')
 
-const server = https.createServer({
-  cert: fs.readFileSync('certificate.pem'),
-  key: fs.readFileSync('key.pem')
-});
-const wss = new WebSocket.Server({ server });
+var privateKey  = fs.readFileSync('key.pem', 'utf8');
+var certificate = fs.readFileSync('cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+//... bunch of other express stuff here ...
+
+//pass in your express app and credentials to create an https server
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8443);
+
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({
+    server: httpsServer
+  });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
@@ -15,5 +26,3 @@ wss.on('connection', function connection(ws) {
 
   ws.send('something');
 });
-
-server.listen(8080);
